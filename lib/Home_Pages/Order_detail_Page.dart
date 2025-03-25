b2db2d0ae71654368e69567a6706eb769692baec
Durgeshpatel77 <durgeshzalavadiya@gmail.com>
2025-03-25 16:23:07 +0530

    connect firebase

--- lib/Home_Pages/Order_detail_Page.dart
+++ lib/Home_Pages/Order_detail_Page.dart
@@ -18,54 +18,126 @@ class _OrderDetailPageState extends State<OrderDetailPage> {
   Widget build(BuildContext context) {
     final width = MediaQuery.of(context).size.width;
     final height = MediaQuery.of(context).size.height;
-    return  Scaffold(
+    return Scaffold(
       body: Stack(
         children: [
           CustomGradientContaienrtwo(
             width: width * width,
             height: height * height,
           ),
-      Container(
-        height: height * height,
-        width: width*width,
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: [
-              Colors.white.withOpacity(1),
-              Colors.white.withOpacity(1),
-              Colors.white.withOpacity(1),
-              Colors.white.withOpacity(0),
-            ],
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
+          Container(
+            height: height * height,
+            width: width * width,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Colors.white.withOpacity(1),
+                  Colors.white.withOpacity(1),
+                  Colors.white.withOpacity(1),
+                  Colors.white.withOpacity(0),
+                ],
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+              ),
+            ),
           ),
-        ),
-      ),
           SingleChildScrollView(
-            padding:  EdgeInsets.only(left: width*0.04,top: height*0.04,right: width*0.04),
+            padding: EdgeInsets.only(
+                left: width * 0.04, top: height * 0.04, right: width * 0.04),
             child: Column(
               children: [
                 Container(
                   child: Row(
                     children: [
                       SizedBox(
-                        child: SvgPicture.asset(ImageConstant.Back,
+                        child: SvgPicture.asset(
+                          ImageConstant.Back,
                           fit: BoxFit.fill,
                         ),
                       ),
                       const SizedBox(width: 9),
-                      CustomText(text: "Order details",color: ColorConst.black,fontSize:24,fontWeight: FontWeight.w700,),
+                      CustomText(
+                        text: "Order details",
+                        color: ColorConst.black,
+                        fontSize: 24,
+                        fontWeight: FontWeight.w700,
+                      ),
                     ],
                   ),
                 ),
-                SizedBox(height:30 ,),
-                DesignColorCoustomContainer(child: Column(children: [Text("Hello")],),)
+                SizedBox(
+                  height: 30,
+                ),
+                DesignColorCoustomContainer(
+                  child: Padding(
+                    padding:  EdgeInsets.only(left: 16,right: 16,top: 10,bottom: 10),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          child: Column(
+                            children: [
+                              CustomText(
+                                text: "Order date",
+                                color: ColorConst.dimGray,
+                                fontSize: 14,
+                                fontWeight: FontWeight.w400,
+                              ),
+                              SizedBox(height: 6,),
+                              CustomText(
+                                text: "Order code",
+                                color: ColorConst.dimGray,
+                                fontSize: 14,
+                                fontWeight: FontWeight.w400,
+                              ),
+                              SizedBox(height: 6,),
+                              CustomText(
+                                text: "Order total",
+                                color: ColorConst.dimGray,
+                                fontSize: 14,
+                                fontWeight: FontWeight.w400,
+                              ),
+                            ],
+                          ),
+                        ),
+                            Container(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  CustomText(
+                                    text: "19-Nov-2024",
+                                    color: ColorConst.black,
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.w400,
+                                  ),
+                                  SizedBox(height: 5,),
+                                  CustomText(
+                                    text: "232DH3",
+                                    color: ColorConst.easternBlue,
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.w700,
+                                  ),
+                                  SizedBox(height: 5,),
+                                  CustomText(
+                                    text: "₹100,000 (2 Items)",
+                                    color: ColorConst.black,
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+
+
+                    ),
+                  ),
               ],
             ),
           )
         ],
       ),
-
     );
   }
 }
